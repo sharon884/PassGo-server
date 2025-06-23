35bf3d11c1574c3481223adb686ef33fe0f23e6a
Sharon <sharonsharon9048461583@gmail.com>
2025-06-23 17:19:23 +0530

    stated to implementing google map api service

--- routes/hostRoutes/hostEventMangementRoutes.js
+++ routes/hostRoutes/hostEventMangementRoutes.js
@@ -1,16 +1,16 @@
 // //Host Event Management related routes
-// const express = require("express");
-// const router = express.Router();
-// const { createEvent, getHostEvents, getEventDetails, updateEvent } = require("../../controllers/hostController/hostEventManagementController");
-// const  verifyTokenMiddleware  = require("../../middlewares/verifyTokenMiddleware");
-// const  validateEventData  = require("../../middlewares/hostRelatedMiddlewares/hostEventAddingValidationMiddleware");
+const express = require("express");
+const router = express.Router();
+const { createEvent, getHostEvents, getEventDetails, updateEvent } = require("../../controllers/hostController/hostEventManagementController");
+const  verifyTokenMiddleware  = require("../../middlewares/verifyTokenMiddleware");
+const  validateEventData  = require("../../middlewares/hostRelatedMiddlewares/hostEventAddingValidationMiddleware");
 // const {  verifyHostMiddleware } = require("../../middlewares/hostRelatedMiddlewares/verifyHostMiddleware.js");
-// const runValidation = require("../../middlewares/globalMiddleware/globalValidationResultMiddleware")
+const runValidation = require("../../middlewares/globalMiddleware/globalValidationResultMiddleware")
 // const hostOnly = require("../../middlewares/authorizedRoleMiddlewares/wrappers/hostOnly");
 
 
-// router.post( "/eventadd", verifyTokenMiddleware , hostOnly, verifyHostMiddleware,  validateEventData, runValidation, createEvent );
-// router.route("/my-events").get(verifyTokenMiddleware, hostOnly,verifyHostMiddleware, getHostEvents )
-// router.route("/:eventId").get(verifyTokenMiddleware, hostOnly,verifyHostMiddleware,getEventDetails)
-// .put(verifyTokenMiddleware, hostOnly, verifyHostMiddleware, validateEventData, runValidation, updateEvent )
-// module.exports = router;
+router.post( "/eventadd", verifyTokenMiddleware , validateEventData, runValidation, createEvent );
+router.route("/my-events").get(verifyTokenMiddleware, getHostEvents )
+router.route("/:eventId").get(verifyTokenMiddleware,getEventDetails)
+.put(verifyTokenMiddleware, validateEventData, runValidation, updateEvent )
+module.exports = router;
--- routes/hostRoutes/hostProfileRoutes.js
+++ routes/hostRoutes/hostProfileRoutes.js
@@ -1,18 +1,18 @@
-// //Host Profile related Routes
-// const express = require('express');
-// const router = express.Router();
-// const { getHostProfile, getHostSidebarDetails } = require("../../controllers/hostController/hostProfileController.JS");
-// const verifyToken = require("../../middlewares/verifyTokenMiddleware");
-// const { updateHostProfile } = require("../../controllers/hostController/hostProfileController.JS")
-// const { updatePasswordHost } = require('../../controllers/hostController/hostProfileController.JS');
-// const { requestVerificationHost } = require("../../controllers/hostController/hostProfileController.JS");
-// const hostOnly = require("../../middlewares/authorizedRoleMiddlewares/wrappers/hostOnly");
-// // const  updateHostProfile = require("../../controllers/hostController/hostProfileController.JS");
+//Host Profile related Routes
+const express = require('express');
+const router = express.Router();
+const { getHostProfile, getHostSidebarDetails } = require("../../controllers/hostController/hostProfileController.JS");
+const verifyToken = require("../../middlewares/verifyTokenMiddleware");
+const { updateHostProfile } = require("../../controllers/hostController/hostProfileController.JS")
+const { updatePasswordHost } = require('../../controllers/hostController/hostProfileController.JS');
+const { requestVerificationHost } = require("../../controllers/hostController/hostProfileController.JS");
+const hostOnly = require("../../middlewares/authorizedRoleMiddlewares/wrappers/hostOnly");
+const  updateHostProfile = require("../../controllers/hostController/hostProfileController.JS");
 
-// router.get("/get-host-Profile", verifyToken, hostOnly, getHostProfile);
-// router.route("/update-password").patch( verifyToken, hostOnly, updatePasswordHost);
-// router.route("/update-host-Profile").put( verifyToken, hostOnly, updateHostProfile );
-// router.route("/account/verify-request").post( verifyToken, hostOnly, requestVerificationHost );
-// router.route("/get-details").get( verifyToken, hostOnly, getHostSidebarDetails );
+router.get("/get-host-Profile", verifyToken, hostOnly, getHostProfile);
+router.route("/update-password").patch( verifyToken, hostOnly, updatePasswordHost);
+router.route("/update-host-Profile").put( verifyToken, hostOnly, updateHostProfile );
+router.route("/account/verify-request").post( verifyToken, hostOnly, requestVerificationHost );
+router.route("/get-details").get( verifyToken, hostOnly, getHostSidebarDetails );
 
-// module.exports = router;
+module.exports = router;
