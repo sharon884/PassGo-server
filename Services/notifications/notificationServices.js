fecb5567573988377bb742c0220a8d41eefd4c7a
Sharon <sharonsharon9048461583@gmail.com>
2025-07-20 15:40:02 +0530

    testing

--- Services/notifications/notificationServices.js
+++ Services/notifications/notificationServices.js
@@ -9,6 +9,7 @@ const createNotification = async (io, { userId, role, type, message, reason }) =
     reason,
   });
 
+  console.log(userId);
   io.to(userId.toString()).emit("new-notification", notification);
   return notification;
 };
