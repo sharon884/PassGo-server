d3b8ecc798fd5330442e326fa523f5d16407e6d1
Sharon <sharonsharon9048461583@gmail.com>
2025-07-30 12:37:02 +0530

    implemented the admin side event cancellation route setup

--- routes/adminRoutes/admineventcancellationRoutes.js
+++ routes/adminRoutes/admineventcancellationRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { getPendingCancellationRequests } = require("../../controllers/adminController/eventcancellationController");
 const  verifyToken = require("../../middlewares/verifyTokenMiddleware");
+const { approveCancellationRequest } = require("../../controllers/adminController/eventcancellationController");
 
 const router = express.Router();
 
 router.route("/pending").get( verifyToken, getPendingCancellationRequests);
+router.route("/approve/:requestId").post( verifyToken, approveCancellationRequest );
 
 module.exports = router;
