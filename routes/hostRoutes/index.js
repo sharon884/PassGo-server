e8fd278ae7bc17a86fb1f14dcb6af57c8ee9c49c
Sharon <sharonsharon9048461583@gmail.com>
2025-07-30 11:13:40 +0530

    added route for the host event cancellation request

--- routes/hostRoutes/index.js
+++ routes/hostRoutes/index.js
@@ -3,16 +3,17 @@ const router = express.Router();
 
 // const hostAuthRoutes = require("./hostAuthRoutes");
 const hostEventMangementRoutes = require("./hostEventMangementRoutes");
-const hostPaymentRoutes  = require("./hostPaymentRoutes");
+const hostPaymentRoutes = require("./hostPaymentRoutes");
 const hostOfferRoutes = require("./hostOfferRoutes");
 const hostWalletRoutes = require("./hostWalletRoutes");
-
+const hostcancellationRoutes = require("./hostcancellationRoutes");
 
 // router.use("/auth", hostAuthRoutes); //Routes related to host Authentication
-// router.use("/profile", hostProfileRoutes);  //Routes related to host Profile 
-router.use("/event", hostEventMangementRoutes );  //Routes related to host Event Mangement 
-router.use("/payment", hostPaymentRoutes );
+// router.use("/profile", hostProfileRoutes);  //Routes related to host Profile cancellationRoutes.js
+router.use("/event", hostEventMangementRoutes); //Routes related to host Event Mangement
+router.use("/payment", hostPaymentRoutes);
 router.use("/offer", hostOfferRoutes);
 router.use("/wallet", hostWalletRoutes);
+router.use("/event/cancellation", hostcancellationRoutes);
 
 module.exports = router;
