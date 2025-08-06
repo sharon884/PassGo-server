91e45d57fb217bc5a80cd6f89c902356225264a0
Sharon <sharonsharon9048461583@gmail.com>
2025-08-06 19:17:05 +0530

    Initial backend commit with Docker setup

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use("/api/landing", landingRoutes);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-  console.log(`server running on http://localhost${port}`);
+  console.log(`server running on http://localhost:${port}`);
 });
 
 module.exports = app;
