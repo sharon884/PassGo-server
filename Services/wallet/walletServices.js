e8de099259aef358d04d3ef325e47a51465f8d78
Sharon <sharonsharon9048461583@gmail.com>
2025-10-03 23:53:34 +0530

    fixed wallet data fecthing issur related null for no wallet

--- Services/wallet/walletServices.js
+++ Services/wallet/walletServices.js
@@ -5,44 +5,61 @@ const getUserWallet = async ( userId ) => {
     try {
         const wallet = await Wallet.findOne({ user: userId });
         const transactions = await Transaction.find({ userId : userId, role : "user" }).sort({ createdAt : -1});
-        console.log(transactions)
+        console.log(transactions);
+        
+   
+        if (!wallet) {
+            return { balance: 0, transactions: transactions };
+        }
+        
         return { balance : wallet.balance, transactions };
     } catch ( error ) {
         console.log("error while fetchng wallent and transcation details of user : ",error );
-        throw new Error("error while fetchng wallent and transcation details of user :",error);
+       
+        throw new Error("error while fetchng wallent and transcation details of user: " + error.message);
     }
 }
-
-
 
 
 const getHostWallet = async ( hostId ) => {
     try {
         const wallet = await Wallet.findOne({ user: hostId });
         const transactions = await Transaction.find({ userId : hostId, role : "host"}).sort({ createdAt : -1});
-        console.log(transactions)
+        console.log(transactions);
+    
+        if (!wallet) {
+            return { balance: 0, transactions: transactions };
+        }
+        
         return { balance : wallet.balance, transactions };
     } catch ( error ) {
         console.log("error while fetchng wallent and transcation details of user : ",error );
-        throw new Error("error while fetchng wallent and transcation details of user :",error);
+      
+        throw new Error("error while fetchng wallent and transcation details of user: " + error.message);
     }
 }
 
 
-
 const getAdminWallet = async ( userId ) => {
     try {
-        const wallet = await Wallet.findOne({ walletType: "admin" });
-        console.log(wallet)
+       
+        const wallet = await Wallet.findOne({ walletType: "admin" }); 
+        console.log(wallet);
+        
+        
+        if (!wallet) {
+            console.error("CRITICAL: Admin wallet not found in database.");
+            throw new Error("CRITICAL: Admin wallet not found.");
+        }
+        
         const transactions = await Transaction.find({ userId : userId , walletType : "admin"}).sort({ createdAt : -1});
-         console.log(transactions)
+         console.log(transactions);
         return { balance : wallet.balance, transactions };
     } catch ( error ) {
         console.log("error while fetchng wallent and transcation details of user : ",error );
-        throw new Error("error while fetchng wallent and transcation details of user :",error);
+        throw new Error("error while fetchng wallent and transcation details of user: " + error.message);
     }
 };
-
 
 
 module.exports = {
